a372ef65c15abdb97d434f04506fdaedb2d042a1
Marius Kintel <marius@kintel.net>
2015-09-15 21:24:36 -0400

    OpenSCAD-2015.03-1

--- horn.sh
+++ horn.sh
@@ -1,4 +1,4 @@
-OPENSCAD=~/Desktop/OpenSCAD-2013.10.26.app/Contents/MacOS/OpenSCAD
+OPENSCAD=~/Desktop/OpenSCAD-2015.03-1.app/Contents/MacOS/OpenSCAD
 for i in {0..4}; do
   $OPENSCAD horn.scad -D"ITEM=$i;" -o horn-$i.stl
 done
